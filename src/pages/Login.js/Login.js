c248b38fc487e9c7b680e0e08122b03d32f1c9b6
Richard NSHIMYUMUKIZA <ricahcyuzuzo@gmail.com>
2022-09-28 09:09:06 +0200

    Add login

--- src/pages/Login.js/Login.js
+++ src/pages/Login.js/Login.js
@@ -4,7 +4,7 @@ const Login = () => {
   return (
     <div className='w-full flex justify-center items-center bg-[#182747]'>
         <div className='w-[1300px] h-[1000px] px-[20px] mt-[100px] items-center justify-between'>
-            <h1 className='text-white text-[25px] text-center mb-[30px] font-semibold'>Register</h1>
+            <h1 className='text-white text-[25px] text-center mb-[30px] font-semibold'>Login</h1>
             <div className='w-[700px] min-h-[400px] mx-auto rounded-[20px] bg-[#006494] '>
                 <div className='flex flex-col w-[80%] mx-auto '>
                     <label className='text-white text-[18px] mb-[10px] mt-[30px]'>Email</label>
